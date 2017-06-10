dfde3c9fdd2b99e94dcc7ef11dfff266932191b9
Ederson de Jesus <edersonjseder@hotmail.com>
2017-06-10 18:01:08 -0400

    Added password encryption to Spring security

--- src/main/java/com/devopsbuddy/config/SecurityConfig.java
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -2,13 +2,16 @@ package com.devopsbuddy.config;
 
 import com.devopsbuddy.backend.service.UserSecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,6 +27,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
+
+    /** The encryption SALT. */
+    private static final String SALT = "dfegrumam3lforiy8;4do";
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+    }
 
     /** Public URLs */
     private static final String [] PUBLIC_MATCHES = {
@@ -72,6 +83,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("USER"); */
 
         auth
-                .userDetailsService(userSecurityService);
+                .userDetailsService(userSecurityService)
+                .passwordEncoder(passwordEncoder());
+
     }
 }
